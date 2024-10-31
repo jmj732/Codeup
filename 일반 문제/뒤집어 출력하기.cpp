9464a5d483bc7966307f497b27a68486113c1e50
jaeminjo732 <jaeminjo732@gmail.com>
2024-10-31 16:51:45 +0900

    뒤집어 출력하기.cpp

---
+++ 일반 문제/뒤집어 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::string str;
+    std::cin >> str;
+    for(auto c = str.rbegin(); c != str.rend(); ++c) {
+        std::cout << *c;
+    }
+    return 0;
+}
