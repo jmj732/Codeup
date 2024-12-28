d128dab07c5432381e786f38e21b46e0973af1db
jaeminjo732 <jaeminjo732@gmail.com>
2024-12-28 22:06:01 +0900

    하노이 탑.cpp"

---
+++ 코드업/재귀/하노이 탑 1.py
@@ -0,0 +1,11 @@
+def hanoi(n, a, b, c):
+    if n == 1:
+        print(f"Disk 1 : {a} to {c}")
+    else:
+        hanoi(n-1, a, c, b)
+        print(f"Disk {n} : {a} to {c}")
+        hanoi(n-1, b, a, c)
+
+
+n = int(input())
+hanoi(n, 'A', 'B', 'C')
